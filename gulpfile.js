ff9749809764f2ba6faa7322f874bd6b49264d94
DarkWater <darkwater666@mail.ru>
2015-02-28 15:40:17 +0300

    gulpfile modernize

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
+'use strict';
+
 var gulp = require('gulp'),
-    slim = require('gulp-slim'),
-    stylus = require('gulp-stylus'),
-    coffee = require('gulp-coffee'),
-    livereload = require('gulp-livereload'),
-    imagemin = require('gulp-imagemin'),
-    concat = require('gulp-concat'),
-    connect = require('connect'),
-    serveStatic = require('serve-static'),
-    nib = require('nib');
+	slim = require('gulp-slim'),
+	stylus = require('gulp-stylus'),
+	coffee = require('gulp-coffee'),
+	livereload = require('gulp-livereload'),
+	imagemin = require('gulp-imagemin'),
+	concat = require('gulp-concat'),
+	connect = require('connect'),
+	serveStatic = require('serve-static'),
+	nib = require('nib');
 
 
 gulp.task('stylus', function() {
-    gulp.src('./styl/*.styl')
-    	.pipe(stylus({use: nib(), compress: true}))
-	    .on('error', console.log)
-	    .pipe(gulp.dest('./public/css/'))
-	    .pipe(livereload());
+	gulp.src('./styl/*.styl')
+		.pipe(stylus({use: nib(), compress: true}))
+		.on('error', console.log)
+		.pipe(gulp.dest('./public/css/'));
 });
 
 
@@ -31,27 +32,29 @@ gulp.task('coffee',function(){
 	gulp.src('./coffee/*.coffee')
 		.pipe(coffee({bare: true}))
 		.on('error', console.log)
-	   .pipe(gulp.dest('./public/js'))
-	   .pipe(livereload());
+		.pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('concat', function(){
-  gulp.task('coffee');
-	gulp.src('./public/js/*.js')
-		.pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./public/min/'))
+gulp.task('concat-js', function() {
+	gulp.task('coff');
+	gulp.src('./public/js/scripts.js')
+		.pipe(concat('scripts.min.js'))
+		.pipe(gulp.dest('./public/min/'))
 		.pipe(livereload());
-  gulp.task('styl');
-	gulp.src('./public/css/*.css')
-		.pipe(concat('styles.css'))
-    .pipe(gulp.dest('./public/min/'))
+});
+
+gulp.task('concat-css', function() {
+	gulp.task('styl');
+	gulp.src(['./public/css/styles.css'])
+		.pipe(concat('styles.min.css'))
+		.pipe(gulp.dest('./public/min/'))
 		.pipe(livereload());
 });
 
 gulp.task('imagemin',function(){
-	 gulp.src('./img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./public/img/'));
+	gulp.src('./img/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./public/img/'));
 });
 
 
@@ -59,19 +62,20 @@ gulp.task('server', function() {
 	connect()
 		.use(require('connect-livereload')())
 		.use(serveStatic(__dirname + '/public'))
-	  .listen('3333');
+		.listen('3333');
 
 	console.log('Сервер работает по адресу http://localhost:3333');
 });
 
 gulp.task('watch', function(){
-  livereload.listen();
+	livereload.listen();
 	gulp.watch('./styl/*.styl',['stylus']);
 	gulp.watch('./*.slim',['slim']);
 	gulp.watch('./coffee/*.coffee',['coffee']);
-	gulp.watch(['./public/js/*.js','./public/css/*.css'],['concat']);
+	gulp.watch(['./public/js/*.js'],['concat-js']);
+	gulp.watch(['./public/css/*.css'],['concat-css']);
 	gulp.watch('./img/**/*',['imagemin']);
-  gulp.start('server');
+	gulp.start('server');
 });
 
-gulp.task('default',['watch','stylus','slim','coffee','concat','imagemin']);
+gulp.task('default',['watch','stylus','slim','coffee','concat-js','concat-css','imagemin']);
